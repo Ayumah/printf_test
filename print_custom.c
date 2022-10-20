54b1e137e044b339ad5619921ab46989ed8a2ab5
lembathomas <lembathomas@gmail.comgit config --global user.name lembathomas>
2022-10-20 06:13:21 -0700

    adding print custm file

--- print_custom.c
+++ print_custom.c
@@ -65,7 +65,7 @@ int print_rot13(va_list l, flags_t *f)
 int i, j;
 char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-har *s = va_arg(l, char *);
+char *s = va_arg(l, char *);
 (void)f;
 for (j = 0; s[j]; j++)
 {
@@ -74,10 +74,9 @@ _putchar(s[j]);
 else
 {
 for (i = 0; i <= 52; i++)
-{
 if (s[j] == rot13[i])
 _putchar(ROT13[i]);
-
+}
 }
 }
 return (j);
